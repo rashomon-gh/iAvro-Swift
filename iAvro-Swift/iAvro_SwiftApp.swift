24ba3f611de6e846c587326cd21d22fc17c82a40
rashomon <shawon.ashraf@outlook.com>
2026-04-04 08:33:28 +0200

    Initial Commit

---
+++ iAvro-Swift/iAvro_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAvro_SwiftApp.swift
+//  iAvro-Swift
+//
+//  Created by Shawon Ashraf on 04/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct iAvro_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
