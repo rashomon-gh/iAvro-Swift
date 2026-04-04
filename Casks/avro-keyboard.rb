c2bffe6d9c2407054c5aaf9abaece27a4a7f68bd
Shawon Ashraf <shawon.ashraf@outlook.com>
2026-04-04 19:54:34 +0200

    Update version format for avro-keyboard cask

--- Casks/avro-keyboard.rb
+++ Casks/avro-keyboard.rb
@@ -1,5 +1,5 @@
 cask "avro-keyboard" do
-  version "1.0.0"
+  version "v1.0.0"
   sha256 :no_check
 
   url "https://github.com/rashomon-gh/iAvro-Swift/releases/download/#{version}/Avro-Keyboard-#{version}.zip"
